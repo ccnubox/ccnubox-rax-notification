8b48b7c7a62920e3fa5e0abe188e4cb189bdf9b3
zxc0328 <120797887@qq.com>
2018-08-14 12:01:50 +0800

    feat: add banner plugin

--- webpack.config.update.js
+++ webpack.config.update.js
@@ -1,5 +1,7 @@
 // webpack.config.update.js
 const path = require('path');
+const webpack = require("webpack");
+const package = require("./package.json");
 
 module.exports = function update(webpackConfig) {
   webpackConfig.entry["com.muxistudio.noti.list"] = [
@@ -8,6 +10,14 @@ module.exports = function update(webpackConfig) {
   webpackConfig.entry["com.muxistudio.noti.detail"] = [
     path.resolve(__dirname, "./src/second.js")
   ];
-  console.log("here", webpackConfig);
+  console.log("current env", process.env.NODE_ENV);
+  if (process.env.NODE_ENV === "production") {
+    webpackConfig.plugins[7].options.include = /\.js$/;
+    webpackConfig.plugins.push(
+      new webpack.BannerPlugin({
+        banner: `com.muxistudio.notification, version ${package.version}, built time: ${Date()}`
+      })
+    );
+  }
   return webpackConfig;
 };
